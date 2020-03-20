c605430ba7d6501414f3eed553116d18121cec67
Jacek Wesołowski <jacqu25@yahoo.com>
2020-03-20 20:20:05 +0100

    Continue fun with symfony events

--- src/AppBundle/Listener/LogLoginUserListener.php
+++ src/AppBundle/Listener/LogLoginUserListener.php
@@ -4,7 +4,8 @@ namespace AppBundle\Listener;
 
 use AppBundle\Entity\User;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 /**
  * @author Jacek Wesołowski <jacqu25@yahoo.com>
@@ -13,21 +14,27 @@ class LogLoginUserListener
 {
     private $logger;
 
-    public function __construct(LoggerInterface $logger)
+    private $authorizationChecker;
+
+    public function __construct(LoggerInterface $logger, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->logger = $logger;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
-    public function onKernelResponse(FilterResponseEvent $event): void
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event): void
     {
-        $sessionKey = $event->getRequest()->getSession()->get('_security_main');
-        if (null === $sessionKey) {
-            return;
+        /** @var User $user */
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if ($this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            // user has just logged in
+            $this->logger->info(sprintf('Użytkownik o e-mailu %s właśnie się zalogował ;)', $user->getEmail()));
         }
 
-        $user = unserialize($sessionKey)->getUser();
-        if ($user instanceof User) {
-            $this->logger->info(sprintf('Użytkownik o e-mailu %s właśnie się zalogował ;)', $user->getEmail()));
+        if ($this->authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            // user has logged in using remember_me cookie
+            // can be useful in other app
         }
     }
 }
